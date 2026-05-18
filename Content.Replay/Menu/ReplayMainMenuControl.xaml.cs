5619dd7d23f4bea8b38551bc870630a9ef1f3a5e
IsNotAcceptable <184727191+IsNotAcceptable@users.noreply.github.com>
2026-05-18 14:41:05 +0700

    fix yaml

--- Content.Replay/Menu/ReplayMainMenuControl.xaml.cs
+++ Content.Replay/Menu/ReplayMainMenuControl.xaml.cs
@@ -23,7 +23,7 @@ public sealed partial class ReplayMainMenuControl : Control
         LayoutContainer.SetGrowHorizontal(VBox, LayoutContainer.GrowDirection.Begin);
 
         Subtext.FontOverride = resCache.GetFont("/Fonts/NotoSansDisplay/NotoSansDisplay-Bold.ttf", 24);
-        var logoTexture = resCache.GetResource<TextureResource>("/Textures/_FP/Logo/logo.png"); // Fractal: logo
+        var logoTexture = resCache.GetResource<TextureResource>("/Textures/_Orehum/Logo/logo.png"); // Orehum: logo
         Logo.Texture = logoTexture;
 
         LayoutContainer.SetAnchorPreset(InfoContainer, LayoutContainer.LayoutPreset.BottomLeft);
